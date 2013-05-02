de4ee62a8c548cfaf5d8150273e4f0f881869c59
André Freitas <p.andrefreitas@gmail.com>
2013-05-02 09:35:20 +0100

    Criado o teste básico do ENFA

--- enfa/TestENFA.java
+++ enfa/TestENFA.java
@@ -10,33 +10,45 @@ public class TestENFA extends TestCase {
 
 	public void test_constructor_match() throws Exception{
 
+		/* States */
 		Set<String> states = new TreeSet<String>();
+		states.add("q0");
 		states.add("q1");
 		states.add("q2");
-
+		states.add("q3");
+		states.add("q4");
+		
+		/* Alphabet */
 		Set<String> alphabet = new TreeSet<String>();
 		alphabet.add("a");
 		alphabet.add("b");
 
+		/* Transitions */
 		ArrayList<String[]> transitions = new ArrayList<String[]>();
-		String[] t1 = { "q1", "a", "q2" };
-		String[] t2 = { "q2", "b", "q2" };
+		String[] t1 = { "q0", "a", "q2" };
+		String[] t2 = { "q0", "a", "q1" };
+		String[] t3 = { "q2", "", "q3" };
+		String[] t4 = { "q3", "b", "q4" };
 		transitions.add(t1);
 		transitions.add(t2);
+		transitions.add(t3);
+		transitions.add(t4);
 
-		String initial_state = "q1";
-
+		/* Initial State */
+		String initial_state = "q0";
+		
+		/* Accept States */
 		Set<String> accept_states = new TreeSet<String>();
-		accept_states.add("q2");
+		accept_states.add("q1");
+		accept_states.add("q4");
+		
+		/* Create ENFA */
 		ENFA d1 = new ENFA(states, alphabet, transitions, initial_state,
 				accept_states);
 		
+		/* Test */
 		assertTrue(d1.match("a.b"));
-		assertTrue(d1.match("a.b.b.b.b.b"));
-		assertFalse(d1.match("a.c"));
-		assertFalse(d1.match("j.h.g.r.u.i.g.h.e.r.u.i.h"));
-		assertFalse(d1.match(""));
-		assertFalse(d1.match("a.a.a.a.b.b.b.b.b.b"));
+
 	}
 
 
