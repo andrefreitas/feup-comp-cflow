025ff63171a085b5ef1341c9c5de6c3ac88aea80
André Freitas <p.andrefreitas@gmail.com>
2013-05-16 09:17:32 +0100

    Já manipula o cflow.

--- Cflow.java
+++ Cflow.java
@@ -1,59 +1,73 @@
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.TreeSet;
 
 import preprocessor.PreProcessor;
 import enfa.ENFA;
 import parser.RegexParser;
+
 public class Cflow {
 	public static ENFA automata;
 	public static TreeSet<String> states = new TreeSet<String>();
 	public static ArrayList<String[]> log = new ArrayList<String[]>();
+
 	public static void main(String args[]) {
 		// (1) Convert file
 		PreProcessor myFile = new PreProcessor();
+		
+		// (2) Create cflow dir
+		try{
+			Runtime.getRuntime().exec("cmd /C mkdir cflow");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// (3) Convert files
 		myFile.run(args);
-		// (2) Convert Regex to ENFA
-		String regex = args[0]+"\n";
+		
+		try {
+			Process p = Runtime.getRuntime().exec("cmd /C javac cflow\\*.java");
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// (2) Set initial state
+
+	}
+
+	public static void start(String regex) {
+		Cflow.states.add(Cflow.automata.get_initial_state());
 		InputStream is = new ByteArrayInputStream(regex.getBytes());
 		RegexParser parser = new RegexParser(is);
 		Cflow.automata = parser.getENFA();
-		
-		// (3) Set initial state
-		Cflow.states.add(Cflow.automata.get_initial_state());
-		
-		//Test
-		transition("a");
-		transition("a");
-		transition("a");
-		transition("b");
-		transition("c");
-		transition("d");
-		transition("a");
-		
-		showResult();		
 	}
-	
+
 	public static void transition(String block) {
 		states = Cflow.automata.step_forward(Cflow.states, block);
 		if (states.isEmpty()) {
-			String[] message = {block, "failed"};
+			String[] message = { block, "failed" };
 			log.add(message);
-		}
-		else {
-			String[] message = {block, "passed"};
+		} else {
+			String[] message = { block, "passed" };
 			log.add(message);
 		}
 	}
-	
+
 	public static void showResult() {
-		for (String[] message: log) {
-			System.out.println(message[0]+": " + message[1]);	
+		for (String[] message : log) {
+			System.out.println(message[0] + ": " + message[1]);
 		}
-		
-		for (String state: states) {
-			String[] identifiers = {""};
+
+		for (String state : states) {
+			String[] identifiers = { "" };
 			if (Cflow.automata.matchRecursive(state, identifiers, 0)) {
 				System.out.println("Flow accepted.");
 				return;
--- preprocessor/PreProcessor.java
+++ preprocessor/PreProcessor.java
@@ -14,7 +14,7 @@ public class PreProcessor {
 	public void run(String[] args) {
 		for(int i = 1; i < args.length; i++) {
 			String input = read_file(args[i]);
-		    write_new_file(args[i].substring(0, args[i].lastIndexOf('.'))+"_cflow.java", input);
+		    write_new_file("cflow/" + args[i], input);
 		}
 	}
 	
